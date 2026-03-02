c27f1b407e504dbcbe55581e8d49284b29700fee
JoaoMaganin <joaovitormontemor138@gmail.com>
2026-03-02 19:42:13 -0300

    rebuild: rebuild the project from zero

--- erp/src/main/java/br/com/maganinautomec/erp/repository/EstoqueProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.maganinautomec.erp.repository;
-
-import br.com.maganinautomec.erp.entity.EstoqueProdutoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstoqueProdutoRepository extends JpaRepository<EstoqueProdutoEntity, Long> {
-}
--- front-erp/service/EstoqueProdutoService.tsx
+++
@@ -1,9 +0,0 @@
-import { BaseService } from './BaseService';
-
-export class EstoqueProdutoService extends BaseService{
-
-    constructor() {
-        super("/api/estoqueProduto");
-    }
-    
-}
